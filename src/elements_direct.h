506e625869e72d692c1c87b5d591c45556ed0132
Daniel Tamayo <tamayo.daniel@gmail.com>
2015-08-19 16:38:30 -0400

    changed names

---
+++ src/elements_direct.h
@@ -0,0 +1,3 @@
+#include "rebound.h"
+
+void rebxf_elements_direct(struct reb_simulation* const sim);
---
+++ src/elements_forces.h
@@ -0,0 +1,3 @@
+#include "rebound.h"
+
+void rebxf_elements_forces(struct reb_simulation* const sim);
