235112a9f0ca570c53e9909caea48159aa3ebd04
Hanno Rein <hanno@hanno-rein.de>
2015-08-29 16:13:43 -0600

    copy

---
+++ src/copyfiles.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Copying file from $REB_DIR"
+cp $REB_DIR/src/tools.h .
+cp $REB_DIR/src/tools.c .
+cp $REB_DIR/src/tree.h .
+cp $REB_DIR/src/tree.c .
+cp $REB_DIR/src/rebound.h .
+
+
