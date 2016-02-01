6b0712b03ac7f60888aea3c116456e451237c12a
Daniel Tamayo <tamayo.daniel@gmail.com>
2016-02-01 15:19:14 -0500

    removed old params

--- src/reboundx.h
+++ src/reboundx.h
@@ -117,18 +117,6 @@ void rebx_add_custom_forces(struct rebx_extras* rebx, void (*custom_forces)(stru
  */
 
 // Getter setter landmark for add_param.py
-void rebx_set_tau_a(struct reb_particle* p, double value); 
-double rebx_get_tau_a(struct reb_particle* p);
-void rebx_set_tau_e(struct reb_particle* p, double value); 
-void rebx_set_tau_inc(struct reb_particle* p, double value); 
-void rebx_set_tau_omega(struct reb_particle* p, double value); 
-void rebx_set_tau_Omega(struct reb_particle* p, double value); 
-double rebx_get_tau_e(struct reb_particle* p);
-double rebx_get_tau_inc(struct reb_particle* p);
-double rebx_get_tau_omega(struct reb_particle* p);
-double rebx_get_tau_Omega(struct reb_particle* p);
-void rebx_set_beta(struct reb_particle* p, double value);
-double rebx_get_beta(struct reb_particle* p);
 
 /** @} */
 
