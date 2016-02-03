34cd8024cfc4058b4d3e8214eb6f08b2ca1e32c2
Daniel Tamayo <tamayo.daniel@gmail.com>
2016-02-03 09:36:12 -0500

    tests
    Former-commit-id: 53bc4291264e373d4353d93d29d3231c76b5da64

--- src/radiation_forces.c
+++ src/radiation_forces.c
@@ -59,8 +59,8 @@ void rebx_radiation_forces(struct reb_simulation* const sim, struct rebx_effect*
 
         // Equation (5) of Burns, Lamy & Soter (1979)
 
-		particles[i].ax += a_rad*((1.-rdot/c)*dx/dr - dvx/c);
-		particles[i].ay += a_rad*((1.-rdot/c)*dy/dr - dvy/c);
-		particles[i].az += a_rad*((1.-rdot/c)*dz/dr - dvz/c);
+        particles[i].ax += a_rad*((1.-rdot/c)*dx/dr - dvx/c);
+        particles[i].ay += a_rad*((1.-rdot/c)*dy/dr - dvy/c);
+        particles[i].az += a_rad*((1.-rdot/c)*dz/dr - dvz/c);
 	}
 }
