bc47072c0f523e9df71f802482aa4709b3229624
Daniel Tamayo <tamayo.daniel@gmail.com>
2016-02-16 17:45:43 -0500

    added include guards

--- src/gr_full.h
+++ src/gr_full.h
@@ -22,9 +22,12 @@
  * along with rebound.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
+#ifndef REBX_GR_FULL_H
+#define REBX_GR_FULL_H
 
 struct reb_simulation;
 struct rebx_effect;
 
 void rebx_gr_full(struct reb_simulation* const sim, struct rebx_effect* const gr);
 
+#endif
