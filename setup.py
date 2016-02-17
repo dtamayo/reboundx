72815ba7bc06ab5ef3335c51e3ac36a535a5a336
Daniel Tamayo <tamayo.daniel@gmail.com>
2016-02-17 16:17:29 -0500

    version 2.7.0

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='reboundx',
-    version='2.6.0',
+    version='2.7.0',
     description='A library for including additional forces in REBOUND',
     long_description=long_description,
     url='http://github.com/dtamayo/reboundx',
