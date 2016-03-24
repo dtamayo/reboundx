130e4504005caf6923762e414e8518d2c8344f89
Daniel Tamayo <tamayo.daniel@gmail.com>
2016-03-24 17:03:20 -0400

    2.8.2
    Former-commit-id: 06eadcc9f5fde9a998fb6b120b4865688f1d2ef6

--- doc/index.rst
+++ doc/index.rst
@@ -3,7 +3,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to REBOUNDx (2.8.1)
+Welcome to REBOUNDx (2.8.2)
 ===========================
 
 .. image:: images/hlTau.png
--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='reboundx',
-    version='2.8.1',
+    version='2.8.2',
     description='A library for including additional forces in REBOUND',
     long_description=long_description,
     url='http://github.com/dtamayo/reboundx',
