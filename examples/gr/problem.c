bd4faf1864d4c4d8e06819fe0064ba2a7ad1baa3
Daniel Tamayo <tamayo.daniel@gmail.com>
2016-07-08 14:08:15 -0400

    latest changes
    Former-commit-id: bdcfe99cd047e448a47a52e571797cb3b38ee2d0

--- examples/gr/problem.c
+++ examples/gr/problem.c
@@ -14,36 +14,34 @@
 
 int main(int argc, char* argv[]){
     struct reb_simulation* sim = reb_create_simulation();
+    struct rebx_extras* rebx = rebx_init(sim);
     sim->dt = 1.e-8;
 
-    struct reb_particle p = {0}; 
-    p.m     = 1.;   
-    reb_add(sim, p); 
+    struct reb_particle* star = reb_add_particle(sim);
+    star->m     = 1.;   
+    star->hash  = reb_tools_hash("star");
 
     double m = 0.;
     double a = 1.e-4; // put planet close to enhance precession so it's visible in visualization (this would put planet inside the Sun!)
     double e = 0.2;
     double omega = 0.;
     double f = 0.;
-
-    struct reb_particle p1 = reb_tools_orbit2d_to_particle(sim->G, p, m, a, e, omega, f);
-    reb_add(sim,p1);
+    
+    struct reb_particle* planet = reb_add_particle(sim);
+    reb_init_particle_from_orbit2d(planet, star, m, a, e, omega, f);
+    planet->hash = reb_tools_hash("planet");
     reb_move_to_com(sim);
     
-    sim->particles[0].hash = reb_tools_hash("star");
-    sim->particles[1].hash = reb_tools_hash("planet");
-
-    struct rebx_extras* rebx = rebx_init(sim);
-
     // Could also add "gr" or "gr_full" here.  See documentation for details.
     struct rebx_effect* gr = rebx_add_effect(rebx, "gr_potential");
    
     // Have to set speed of light in right units (set by G & initial conditions).  Here we use default units of AU/(yr/2pi)
     rebx_set_param_double(gr, "c", C_DEFAULT);  
     // Need to set gr_source param to 1 on the massive particle for any effect (except for "gr_full" where all particles act as sources).
-    rebx_set_param_int(reb_get_particle_by_name(sim, "star"), "gr_source", 1);
+    rebx_set_param_int(planet, "gr_source", 1);
     
     double tmax = 5.e-2;
     reb_integrate(sim, tmax); 
     rebx_free(rebx);    // this explicitly frees all the memory allocated by REBOUNDx 
+    reb_free(sim);
 }
