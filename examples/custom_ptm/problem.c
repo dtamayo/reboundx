d456f6b03c09fcfaa2ecba92a507600e8da2e16b
Daniel Tamayo <tamayo.daniel@gmail.com>
2016-07-19 17:18:32 -0400

    updated custom effect adders and C/python example

--- examples/custom_ptm/problem.c
+++ examples/custom_ptm/problem.c
@@ -11,23 +11,35 @@
 #include "rebound.h"
 #include "reboundx.h"
 
-// A very simple post timestep modification to change the planet's orbit
-// The custom function *has* to take (struct reb_simulation* const sim, struct rebx_effect* const effect)
-// (you can just ignore the effect struct in your function body if you don't want to use it)
+/* A very simple post timestep modification to change the planet's orbit
+ * All custom function *have* to take (struct reb_simulation* const sim, struct rebx_effect* const effect)
+ * (you can just ignore the effect struct in your function body if you don't want to use it)
+ */
 
-void simple_drag(struct reb_simulation* const sim, struct rebx_effect* const effect);
+// for a post_timestep_modification we update the particle states (positions, velocities, masses etc.)
+void simple_drag(struct reb_simulation* const sim, struct rebx_effect* const effect)
+{
+    double* c = rebx_get_param_double(effect, "c"); // get parameters we want user to set
+    double dt = sim->dt_last_done;  // important to check dt for integrators like IAS15 with adaptive timesteps
 
-// Each effect has its own parameters structure.  You can define your own:
-struct custom_params{
-    double coefficient;
-};
+    if(c != NULL){ // check to make sure c is set to avoid segmentation fault if not!
+        sim->particles[2].vx *= 1. - (*c)*dt;
+        sim->particles[2].vy *= 1. - (*c)*dt;
+        sim->particles[2].vz *= 1. - (*c)*dt;
+    }
+}
+
+// for a force we have to update the particle accelerations
+void stark_force(struct reb_simulation* const sim, struct rebx_effect* const effect){
+    double* c = rebx_get_param_double(effect, "c"); // get parameters we want user to set
+
+    if(c != NULL){ // check to make sure c is set to avoid segmentation fault if not!
+        sim->particles[2].ax += (*c); // make sure you += not =, which would overwrite other accelerations
+    }
+}
 
 int main(int argc, char* argv[]){
     struct reb_simulation* sim = reb_create_simulation();
-    // Setup constants
-    sim->dt             = 0.012;    // initial timestep.
-    sim->integrator = REB_INTEGRATOR_IAS15;
-
     struct reb_particle p = {0}; 
     p.m     = 1.;   
     reb_add(sim, p); 
@@ -47,26 +59,19 @@ int main(int argc, char* argv[]){
 
     struct rebx_extras* rebx = rebx_init(sim);  // first initialize rebx
 
-    struct custom_params* params = malloc(sizeof(*params));
-    params->coefficient = 1.e-5;
+    /* We now add our custom post_timestep_modification
+     * We pass rebx, a name, and the function that should be called.
+     * For a custom force, we also have to pass force_is_velocity_dependent,
+     * which should be 1 if our function uses particle velocities, and 0 otherwise. 
+     */
+
+    struct rebx_effect* effect = rebx_add_custom_post_timestep_modification(rebx, "simple_drag", simple_drag);
+    //struct rebx_effect* effect = rebx_add_custom_force(rebx, "stark_force", stark_force, 0);
     
-    // We now add our custom post_timestep_modification
-    // We pass rebx, the function that should be called, and a pointer to our params structure.
-    // You don't have to use a params structure (you would just pass NULL for the last argument).
-    // In this example, you could hardcode 1.e-5 in simple_drag and not use a params structure.
-    rebx_add_custom_post_timestep_modification(rebx, simple_drag, params);
+    rebx_set_param_double(effect, "c", 1.e-5);  // we wrote both our functions to read a parameter c, so we set it.
     
     double tmax = 5.e4;
     reb_integrate(sim, tmax);
     rebx_free(rebx);                // Free all the memory allocated by rebx
 }
 
-void simple_drag(struct reb_simulation* const sim, struct rebx_effect* const effect)
-{
-    struct custom_params* params = effect->paramsPtr;
-    double c = params->coefficient;
- 
-    sim->particles[2].vx *= 1. - c*sim->dt_last_done;
-    sim->particles[2].vy *= 1. - c*sim->dt_last_done;
-    sim->particles[2].vz *= 1. - c*sim->dt_last_done;
-}
