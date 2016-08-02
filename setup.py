41fec5bb1b4d5cbeefc1020664a015b74c72b4bc
Daniel Tamayo <tamayo.daniel@gmail.com>
2016-08-02 17:01:51 -0400

    2.11.1
    Former-commit-id: 39e84b5d2ab0b1cc1fe1d89c89b3504daa9d1c06

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='reboundx',
-    version='2.11.0',
+    version='2.11.1',
     description='A library for including additional forces in REBOUND',
     long_description=long_description,
     url='http://github.com/dtamayo/reboundx',
