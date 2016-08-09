0c1c3735ea6dacbc7ab94e2f08f88e61d09b4bb6
Daniel Tamayo <tamayo.daniel@gmail.com>
2016-08-09 10:37:34 -0400

    updated python for new getters and setters
    Former-commit-id: 9e13850bec303b5bce38167ce01caedd335bc0c3

--- reboundx/params.py
+++ reboundx/params.py
@@ -1,6 +1,6 @@
 import rebound
 from collections import MutableMapping
-from .extras import rebx_param
+from .extras import Param, Effect
 from . import clibreboundx
 from ctypes import byref, c_double, c_int, c_char_p, POINTER, cast
 from numpy.ctypeslib import as_ctypes
@@ -13,10 +13,15 @@ class Params(MutableMapping):
                 raise AttributeError("Need to attach reboundx.Extras instance to simulation before setting particle params.")
 
         self.parent = parent
+        rebx_object_type = {"REBX_TYPE_EFFECT":0, "REBX_TYPE_PARTICLE":1} # corresponds to enum in C
+        if type(parent) == Effect:
+            self.classtype = rebx_object_type["REBX_TYPE_EFFECT"]
+        elif type(parent) == rebound.Particle:
+            self.classtype = rebx_object_type["REBX_TYPE_PARTICLE"]
 
     def __getitem__(self, key):
         name = rebound.hash(key).value
-        current = cast(self.parent.ap, POINTER(rebx_param))
+        current = cast(self.parent.ap, POINTER(Param))
         while current:
             param = current.contents
             if param.hash == name:
@@ -40,22 +45,22 @@ class Params(MutableMapping):
             ctypevar = as_ctypes(value)
             ctype = type(ctypevar)
             if ctype == c_int:
-                clibreboundx.rebx_set_param_int(byref(self.parent), c_char_p(key.encode('ascii')), ctypevar)
+                clibreboundx.rebx_set_param_int(byref(self.parent), c_char_p(key.encode('ascii')), ctypevar, self.classtype)
                 return
             elif ctype == c_double:
-                clibreboundx.rebx_set_param_double(byref(self.parent), c_char_p(key.encode('ascii')), ctypevar)
+                clibreboundx.rebx_set_param_double(byref(self.parent), c_char_p(key.encode('ascii')), ctypevar, self.classtype)
                 return
         else:
             if typ == int:
-                clibreboundx.rebx_set_param_int(byref(self.parent), c_char_p(key.encode('ascii')), c_int(value))
+                clibreboundx.rebx_set_param_int(byref(self.parent), c_char_p(key.encode('ascii')), c_int(value), self.classtype)
                 return
             elif typ == float:
-                clibreboundx.rebx_set_param_double(byref(self.parent), c_char_p(key.encode('ascii')), c_double(value))
+                clibreboundx.rebx_set_param_double(byref(self.parent), c_char_p(key.encode('ascii')), c_double(value), self.classtype)
                 return
         raise AttributeError("Data type for {0} param not supported.".format(key))
 
     def __delitem__(self, key):
-        success = clibreboundx.rebx_remove_param(byref(self.parent), c_char_p(key.encode('ascii')))
+        success = clibreboundx.rebx_remove_param(byref(self.parent), c_char_p(key.encode('ascii')), self.classtype)
         if not success:
             raise AttributeError("{0} parameter to delete not found.".format(key))
 
@@ -63,7 +68,7 @@ class Params(MutableMapping):
         raise AttributeError("Iterator for params not implemented.")
 
     def __len__(self):
-        current = cast(self.parent.ap, POINTER(rebx_param))
+        current = cast(self.parent.ap, POINTER(Param))
         ctr = 0
         while current:
             ctr += 1
