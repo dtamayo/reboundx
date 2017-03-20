924d1981ededdde65fe4a8c41a4daac67ae317a7
Daniel Tamayo <tamayo.daniel@gmail.com>
2017-03-20 17:15:47 -0400

    2.17.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ try:
     ghash = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii")
     ghash_arg = "-DREBXGITHASH="+ghash
 except:
-    ghash_arg = "-DREBXGITHASH=e6f5c19a0403650978cb84fb3b1e377c5e65504a" #GITHASHAUTOUPDATE
+    ghash_arg = "-DREBXGITHASH=4f902b0934d6ca77d7100f2559d5366179a2406e" #GITHASHAUTOUPDATE
 
 class build_ext(_build_ext):
     def finalize_options(self):
@@ -70,7 +70,7 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='reboundx',
-    version='2.17.1',
+    version='2.17.2',
     description='A library for including additional forces in REBOUND',
     long_description=long_description,
     url='http://github.com/dtamayo/reboundx',
