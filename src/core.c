83dfcca993225dace515b267cfce7a80b4d65a9c
Daniel Tamayo <tamayo.daniel@gmail.com>
2017-06-07 14:19:22 -0400

    updated to new rebound transformations.c and renamed integrators

--- src/core.c
+++ src/core.c
@@ -33,8 +33,8 @@
 #include "core.h"
 #include "rebound.h"
 #include "integrator_implicit_midpoint.h"
-#include "euler.h"
-#include "rk4.h"
+#include "integrator_euler.h"
+#include "integrator_rk4.h"
 
 #define STRINGIFY(s) str(s)
 #define str(s) #s
