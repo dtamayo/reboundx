cc3da39d299641f70d5fb837f9d74db72313504b
Daniel Tamayo <tamayo.daniel@gmail.com>
2018-08-13 15:06:03 -0400

    renamed to rebx_additional_forces
    Former-commit-id: a27fc4972132181fc1a274394c1993e84e4497cb

--- src/core.h
+++ src/core.h
@@ -53,7 +53,7 @@ void rebx_free_effects(struct rebx_extras* rebx);           // Frees all effects
  Functions executing forces & ptm each timestep
  *********************************************/
 
-void rebx_forces(struct reb_simulation* sim);                       // Calls all the forces that have been added to the simulation.
+void rebx_additional_forces(struct reb_simulation* sim);                       // Calls all the forces that have been added to the simulation.
 void rebx_pre_timestep_modifications(struct reb_simulation* sim);   // Calls all the pre-timestep modifications that have been added to the simulation.
 void rebx_post_timestep_modifications(struct reb_simulation* sim);  // Calls all the post-timestep modifications that have been added to the simulation.
 
