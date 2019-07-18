16304c83f8cbf2a520821a2acbc7853d7f7bb750
Daniel Tamayo <tamayo.daniel@gmail.com>
2019-07-18 11:28:59 -0400

    added get_extras function

--- reboundx/__init__.py
+++ reboundx/__init__.py
@@ -40,7 +40,7 @@ def params(self):
 
 rebound.Particle.params = params
 
-from .extras import Extras, Param, Node, Force, Operator, integrators
+from .extras import Extras, Param, Node, Force, Operator, integrators, get_extras
 from .tools import coordinates, install_test
 from .params import Params
-__all__ = ["__version__", "__build__", "__githash__", "Extras", "Param", "Effect", "Params", "coordinates", "integrators"] 
+__all__ = ["__version__", "__build__", "__githash__", "Extras", "get_extras", "Param", "Effect", "Params", "coordinates", "integrators"] 
