4225b4db7d91b48a846e1496f954512f21a51e98
Daniel Tamayo <tamayo.daniel@gmail.com>
2019-08-15 23:59:59 -0400

    3.0.0

--- doc/index.rst
+++ doc/index.rst
@@ -3,8 +3,8 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to REBOUNDx (2.20.0)
-============================
+Welcome to REBOUNDx (3.0.0)
+===========================
 
 .. image:: images/hlTau.png
    :width: 200px
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ try:
     ghash = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii")
     ghash_arg = "-DREBXGITHASH="+ghash
 except:
-    ghash_arg = "-DREBXGITHASH=651255eb3d50fad6bb2186af2640567cfaf8e121" #GITHASHAUTOUPDATE
+    ghash_arg = "-DREBXGITHASH=4dd281b3e4015da6e818c0577eb8dbf14aa759a6" #GITHASHAUTOUPDATE
 
 class build_ext(_build_ext):
     def finalize_options(self):
@@ -71,7 +71,7 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='reboundx',
-    version='2.20.0',
+    version='3.0.0',
     description='A library for including additional forces in REBOUND',
     long_description=long_description,
     url='http://github.com/dtamayo/reboundx',
