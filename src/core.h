8821da14c2f35d80f734451aac33700f1f38d8a6
Stanley A. Baronett <51060809+sabaronett@users.noreply.github.com>
2020-01-20 08:50:48 -0800

    Add tides_drag function prototype

--- src/core.h
+++ src/core.h
@@ -74,6 +74,7 @@ void rebx_central_force(struct reb_simulation* const sim, struct rebx_force* con
 void rebx_gravitational_harmonics(struct reb_simulation* const sim, struct rebx_force* const force, struct reb_particle* const particles, const int N);
 void rebx_stark_force(struct reb_simulation* const sim, struct rebx_force* const force, struct reb_particle* const particles, const int N);
 void rebx_migration_force(struct reb_simulation* const sim, struct rebx_force* const force, struct reb_particle* const particles, const int N);
+void rebx_tides_drag(struct reb_simulation* const sim, struct rebx_force* const force, struct reb_particle* const particles, const int N);
 
 /****************************************
  Operator prototypes
