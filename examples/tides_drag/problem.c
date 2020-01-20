ed23659758718997d697457c35f800334d5f09de
Stanley A. Baronett <51060809+sabaronett@users.noreply.github.com>
2020-01-20 08:52:10 -0800

    Update problem.c
    Former-commit-id: bee93c24779dfae68be8080bfd7d52fc719157c4

--- examples/tides_drag/problem.c
+++ examples/tides_drag/problem.c
@@ -1,7 +1,7 @@
 /**
- * Precession from tides.
+ * Drag interaction from tides.
  *
- * This example shows how to add precession due to tides raised on either the primary, the orbiting bodies, or both.
+ * This example shows how to add drag interaction due to slowly rotating tides raised on the primary body.
  * See also the corresponding Python example for more details.
  */
 #include <stdio.h>
@@ -11,46 +11,59 @@
 #include "rebound.h"
 #include "reboundx.h"
 
+void heartbeat(struct reb_simulation* sim);
+double tmax = 100.*2.*M_PI;
+
 int main(int argc, char* argv[]){
     struct reb_simulation* sim = reb_create_simulation();
-    sim->dt = 1.e-5;
+    sim->heartbeat = heartbeat;
+    sim->integrator = REB_INTEGRATOR_IAS15;
 
     struct reb_particle star = {0};
-    star.m     = 1.;   
-    star.hash  = reb_hash("star");
+    star.m     = 1.;
     reb_add(sim, star);
 
-    double m = 1.e-5;   // if planets don't have mass, they won't feel tides.
-    double a = 3.e-2;   // put planet close to enhance precession so it's visible in visualization (this would put planet inside the Sun!)
-    double e = 0.2;
-    double omega = 0.;
-    double f = 0.;
-    
-    struct reb_particle planet = reb_tools_orbit2d_to_particle(sim->G, star, m, a, e, omega, f);
-    planet.hash = reb_hash("planet");
+    struct reb_particle planet = {0};  // add a planet on a circular orbit (with default units where G=1)
+    planet.m = 1e-6;
+    planet.x = 1.;
+    planet.vy = 1.;
     reb_add(sim, planet);
     reb_move_to_com(sim);
     
-    struct rebx_extras* rebx = rebx_attach(sim);
-    struct rebx_force* tides = rebx_load_force(rebx, "tides_precession");
+    struct rebx_extras* rebx = rebx_attach(sim);  // first initialize rebx
+    struct rebx_force* tides = rebx_load_force(rebx, "tides_drag"); // add our new force
+    // rebx_set_param_double(rebx, &sim->particles[1].ap, "migration_tau", 1000); // set particle parameter
     rebx_add_force(rebx, tides);
-   
-    // Have to set R_tides (physical radius) and k1 (apsidal motion constant, half the tidal Love number k2) parameters. 
-    // Could just set one set to consider tides raised on one body only.
 
-    rebx_set_param_double(rebx, &sim->particles[0].ap, "R_tides", 0.005);
-    rebx_set_param_double(rebx, &sim->particles[1].ap, "R_tides", 0.0005);
-    rebx_set_param_double(rebx, &sim->particles[0].ap, "k1", 0.03);
-    rebx_set_param_double(rebx, &sim->particles[1].ap, "k1", 0.3);
+    // Overwrite planet output file w/ col headers
+    system("rm -f planet.txt"); // remove existing file
+    FILE* file = fopen("planet.txt","a");
+    fprintf(file, "Time(yrs)\t\tMass(Msun)\t\tSemi-major Axis(AU)\t\tEccentricity\t\tInclination(Radians)\t\tLongitude_of_Ascending_Node(Radians)\t\tArgument_of_Periapsis(Radians))\t\tTrue_Anomaly(Radians)\n");
+    fclose(file);
+    
+    reb_integrate(sim, tmax);
+}
 
-    /* By default, implementation assumes particles[0] is the primary.
-     * You can also set the tides_primary flag explicitly (don't have to set it to a value):
-     */
+void heartbeat(struct reb_simulation* sim){
+    if (reb_output_check(sim, tmax/1000)){
+        // retrieve Sun particle
+        struct reb_particle sun = sim->particles[0];
+        // retrieve Earth particle
+        struct reb_particle ep = sim->particles[1];
+        struct reb_orbit eo  = reb_tools_particle_to_orbit(sim->G, ep, sun);
+        double t = sim->t;
+        double m = ep.m;
+        double a = eo.a;
+        double e = eo.e;
+        double inc = eo.inc;
+        double Omega = eo.Omega;
+        double omega = eo.omega;
+        double f = eo.f;
+        FILE* file = fopen("planet.txt","a");
 
-    rebx_set_param_int(rebx, &sim->particles[0].ap, "tides_primary", 1);
-
-    double tmax = 1000.;
-    reb_integrate(sim, tmax); 
-    rebx_free(rebx);    // this explicitly frees all the memory allocated by REBOUNDx 
-    reb_free_simulation(sim);
+        reb_output_timing(sim, tmax);
+        reb_integrator_synchronize(sim);
+        fprintf(file,"%e\t\t%e\t\t%e\t\t%e\t\t%e\t\t%e\t\t%e\t\t%e\n",t,m,a,e,inc,Omega,omega,f);
+        fclose(file);
+    }
 }
