6c15418e0fc81a94d760845bce82791fb79a26d5
Daniel Tamayo <tamayo.daniel@gmail.com>
2020-01-24 08:50:07 -0500

    output jac const err
    Former-commit-id: 167e9dd007e4aea78bbf2990f95da488856c6a74

--- examples/ephem_forces/problem.c
+++ examples/ephem_forces/problem.c
@@ -15,6 +15,46 @@
 
 double tmax;
 void heartbeat(struct reb_simulation* r);
+double jac0;
+
+static void ephem(const int i, const double t, double* const m, double* const x, double* const y, double* const z){
+    const double n = 1.;
+    const double mu = 1.e-3;
+    const double m0 = 1.-mu;
+    const double m1 = mu; 
+    if (i==0){
+        *m = m0;
+        const double mfac = -m1/(m0+m1);
+        *x = mfac*cos(n*t);
+        *y = mfac*sin(n*t);
+        *z = 0.;
+    }
+
+    if (i==1){
+        *m = m1;
+        const double mfac = m0/(m0+m1);
+        *x = mfac*cos(n*t);
+        *y = mfac*sin(n*t);
+        *z = 0.;
+    }
+}
+
+double jac(struct reb_simulation* r){
+    const double n = 1;
+    const struct reb_particle* p = &r->particles[0];
+    const double v2 = p->vx*p->vx + p->vy*p->vy;
+    const double coriolis = 2*n*(p->x*p->vy-p->y*p->vx);
+    double m, x, y, z;
+    ephem(0, r->t, &m, &x, &y, &z);
+    const double r1 = sqrt((x-p->x)*(x-p->x) + (y-p->y)*(y-p->y));
+    const double mu1 = r->G*m;
+    ephem(1, r->t, &m, &x, &y, &z);
+    const double r2 = sqrt((x-p->x)*(x-p->x) + (y-p->y)*(y-p->y));
+    const double mu2 = r->G*m;
+    const double j = 2.*(mu1/r1 + mu2/r2) + coriolis - v2;
+    return j;
+}
+
 
 int main(int argc, char* argv[]){
     struct reb_simulation* r = reb_create_simulation();
@@ -42,17 +82,15 @@ int main(int argc, char* argv[]){
 
     tmax            = 10000.;
 
+    jac0 = jac(r);
     reb_integrate(r, tmax);
-    r->dt = 1.e-4;
-    reb_step(r);
-    reb_step(r);
-    reb_step(r);
-
 }
 
 void heartbeat(struct reb_simulation* r){
     if(reb_output_check(r,tmax/10000.)){        // outputs to the screen
         reb_output_timing(r, tmax);
     }
+    const double j = jac(r);
+    fprintf(stderr, "jac const err: %e\n", fabs((j-jac0)/jac0));
 }
 
