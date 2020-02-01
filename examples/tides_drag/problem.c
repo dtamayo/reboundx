2de407f546af03874019782b530ac87e4f49146b
Stanley A. Baronett <51060809+sabaronett@users.noreply.github.com>
2020-02-01 11:48:45 -0800

    Added effect and example w/ hard coded params
    Former-commit-id: fc3abd90b1926b29118451fc1a644dda7a32efd4

--- examples/tides_drag/problem.c
+++ examples/tides_drag/problem.c
@@ -12,27 +12,36 @@
 #include "reboundx.h"
 
 void heartbeat(struct reb_simulation* sim);
-double tmax = 100.*2.*M_PI;
+double tmax;
 
 int main(int argc, char* argv[]){
     struct reb_simulation* sim = reb_create_simulation();
+    // Setup constants
+    sim->dt = 1./20.;              // 1/20 Earth's period (yrs)
+    // sim->dt = 0.0120423;           // 1/20 Mercury's period (yrs)
+    tmax = 8e4;                    // 80 Kyr
+    sim->G = 4*M_PI*M_PI;          // in AU^3 / Msun / yr^2.
+    sim->ri_whfast.safe_mode = 0;  // Turn off safe mode. Need to call reb_integrator_synchronize() before outputs. 
+    sim->ri_whfast.corrector = 11; // 11th order symplectic corrector
+    sim->integrator = REB_INTEGRATOR_WHFAST;
     sim->heartbeat = heartbeat;
-    sim->integrator = REB_INTEGRATOR_IAS15;
 
-    struct reb_particle star = {0};
-    star.m     = 1.;
-    reb_add(sim, star);
-
-    struct reb_particle planet = {0};  // add a planet on a circular orbit (with default units where G=1)
-    planet.m = 1e-6;
-    planet.x = 1.;
-    planet.vy = 1.;
-    reb_add(sim, planet);
-    reb_move_to_com(sim);
+    // Add Sun to sim
+    struct reb_particle sun = {0}; // initialize w/ zeroes
+    sun.m = 0.86;                  // in Msun
+    reb_add(sim, sun);
+    // Add Earth to sim
+    struct reb_orbit eo = {0};
+    double e_mass = .000002988;
+    eo.a = 1.0;                    // in AU
+    struct reb_particle ep = reb_tools_orbit_to_particle(sim->G, sun, e_mass, eo.a, eo.e, eo.inc, eo.Omega, eo.omega, eo.f);
+    reb_add(sim, ep);
     
+    // Add REBOUNDx Additional Effect
     struct rebx_extras* rebx = rebx_attach(sim);  // first initialize rebx
     struct rebx_force* tides = rebx_load_force(rebx, "tides_drag"); // add our new force
-    // rebx_set_param_double(rebx, &sim->particles[1].ap, "migration_tau", 1000); // set particle parameter
+    // struct rebx_force* tides = rebx_load_force(rebx, "migration_force"); // add our new force
+    // rebx_set_param_double(rebx, &sim->particles[1].ap, "migration_tau", 8e4); // set particle parameter
     rebx_add_force(rebx, tides);
 
     // Overwrite planet output file w/ col headers
@@ -41,11 +50,12 @@ int main(int argc, char* argv[]){
     fprintf(file, "Time(yrs)\t\tMass(Msun)\t\tSemi-major Axis(AU)\t\tEccentricity\t\tInclination(Radians)\t\tLongitude_of_Ascending_Node(Radians)\t\tArgument_of_Periapsis(Radians))\t\tTrue_Anomaly(Radians)\n");
     fclose(file);
     
+    reb_move_to_com(sim);
     reb_integrate(sim, tmax);
 }
 
 void heartbeat(struct reb_simulation* sim){
-    if (reb_output_check(sim, tmax/1000)){
+    if (reb_output_check(sim, 1000.)){
         // retrieve Sun particle
         struct reb_particle sun = sim->particles[0];
         // retrieve Earth particle
---
+++ src/tides_drag.c
@@ -0,0 +1,117 @@
+/**
+ * @file    tides_drag.c
+ * @brief   Add drag forces due to slowly rotating tides raised on the primary body.
+ * @author  Stanley A. Baronett <stanley.a.baronett@gmail.com>
+ * 
+ * @section     LICENSE
+ * Copyright (c) 2015 Dan Tamayo, Hanno Rein
+ *
+ * This file is part of reboundx.
+ *
+ * reboundx is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * reboundx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rebound.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The section after the dollar signs gets built into the documentation by a script.  All lines must start with space * space like below.
+ * Tables always must be preceded and followed by a blank line.  See http://docutils.sourceforge.net/docs/user/rst/quickstart.html for a primer on rst.
+ * $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ *
+ * $Tides$       // Effect category (must be the first non-blank line after dollar signs and between dollar signs to be detected by script).
+ *
+ * ======================= ===============================================
+ * Authors                 S.A. Baronett
+ * Implementation Paper    *In progress*
+ * Based on                `Schroder & Smith 2008 <https://arxiv.org/abs/0801.4031>`_.
+ * C Example               :ref:`c_example_tides_drag`.
+ * Python Example          `TidesDrag.ipynb <https://github.com/dtamayo/reboundx/blob/master/ipython_examples/TidesDrag.ipynb>`_.
+ * ======================= ===============================================
+ *
+ * ADD DESCRIPTION HERE
+ * 
+ * **Effect Parameters**
+ * 
+ * None
+ *
+ * **Particle Parameters**
+ *
+ * ============================ =========== ==================================================================
+ * Field (C type)               Required    Description
+ * ============================ =========== ==================================================================
+ * PARAM (float)                Yes         DESCRIPTION
+ * primary (int)                No          Set to 1 to specify the primary.  Defaults to treating particles[0] as primary if not set.
+ * ============================ =========== ==================================================================
+ * 
+ */
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <float.h>
+#include "reboundx.h"
+
+// double tau = 8e4;
+double R0 = 0.78; // Sun physical radius in AU
+double L0 = 869.5; // solar luminosity
+double Omega = 0; // angular velocity of solar rotation
+double lambda2 = 0.023; // depends on properties of convective envelope
+
+void rebx_tides_drag(struct reb_simulation* const sim, struct rebx_force* const force, struct reb_particle* const particles, const int N){
+    //FOR USE LATER W/ ADD'L PARAMETERS
+    // struct rebx_extras* const rebx = sim->extras; // to access add'l params
+    // for (int i=0; i<N; i++){
+    //     const double* migration_tau = rebx_get_param(rebx, particles[i].ap, "migration_tau");
+    //     if (migration_tau != NULL){
+    //         double tau = *migration_tau;
+    //         particles[1].ax -= particles[1].vx/tau;
+    //         particles[1].ay -= particles[1].vy/tau;
+    //         particles[1].az -= particles[1].vz/tau;
+    //     }
+    //     else if (i == 1 && migration_tau == NULL){
+    //         printf("NULL POINTER\n");
+    //     }
+    // }
+
+    struct reb_orbit po = reb_tools_particle_to_orbit(sim->G, particles[1], particles[0]);
+
+    // TRY REPLACING WITH po.d
+    const double dx = particles[1].x - particles[0].x; 
+    const double dy = particles[1].y - particles[0].y;
+    const double dz = particles[1].z - particles[0].z;
+    // const double dr = sqrt(dx*dx + dy*dy + dz*dz); // distance to star
+    const double dr = po.d;
+
+    // TRY REPLACING WITH po.v
+    const double vx = particles[1].vx;
+    const double vy = particles[1].vy;
+    const double vz = particles[1].vz;
+    // const double vmag = sqrt(vx*vx + vy*vy + vz*vz); // magnitude of velocity
+    const double vmag = po.v;
+
+    const double M0 = particles[0].m; // primary mass
+    const double m = particles[1].m; // particle mass
+    const double q = m/M0; // mass ratio
+    const double rratio = R0/dr; // ratio of primary physical radius to orbital radius
+    const double omega = 2.*M_PI/po.P; // angular velocity of orbiting particle
+    const double t_f = cbrt(M0*R0*R0/L0); // convective friction time (Zahn 1989, Eq.15)
+    
+    // Equation (4) of Schroder & Smith (2008)
+    const double torque = 6.*(lambda2/t_f)*q*q*M0*R0*R0*rratio*rratio*rratio*rratio*rratio*rratio*(Omega - omega); // torque from retarded solar bulges (circular orbit)
+
+    // Apply torque to acceleration components
+    particles[1].ax += (torque/(m*dr))*(vx/vmag);
+    particles[1].ay += (torque/(m*dr))*(vy/vmag);
+    particles[1].az += (torque/(m*dr))*(vz/vmag);
+
+    // particles[1].ax -= particles[1].vx/tau;
+    // particles[1].ay -= particles[1].vy/tau;
+    // particles[1].az -= particles[1].vz/tau;
+}
