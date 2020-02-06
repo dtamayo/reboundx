390e7058bfc7d56b78ed4c973c94d4d44e82af7b
Stanley A. Baronett <51060809+sabaronett@users.noreply.github.com>
2020-02-05 22:49:41 -0800

    Added reverse torque and consolidate prefactor
    Former-commit-id: a99e2fe122b54d3a12df3a72ea74e3f7b81b8a43

--- src/tides_drag.c
+++ src/tides_drag.c
@@ -58,13 +58,13 @@
 #include <float.h>
 #include "reboundx.h"
 
-double R0 = 0.78;       // Sun physical radius in AU
+double R0 = 0.78;       // sun's physical radius in AU
 double L0 = 869.5;      // solar luminosity
 double Omega = 0;       // angular velocity of solar rotation
 double lambda2 = 0.023; // depends on properties of convective envelope
 
 void rebx_tides_drag(struct reb_simulation* const sim, struct rebx_force* const force, struct reb_particle* const particles, const int N){
-    //FOR USE LATER W/ ADD'L PARAMETERS
+    //FOR USE LATER W/ REGISTERED PARAMETERS
     // struct rebx_extras* const rebx = sim->extras; // to access add'l params
     // for (int i=0; i<N; i++){
     //     const double* migration_tau = rebx_get_param(rebx, particles[i].ap, "migration_tau");
@@ -81,30 +81,29 @@ void rebx_tides_drag(struct reb_simulation* const sim, struct rebx_force* const 
 
     struct reb_orbit po = reb_tools_particle_to_orbit(sim->G, particles[1], particles[0]);
 
-    // const double dx = particles[1].x - particles[0].x; 
-    // const double dy = particles[1].y - particles[0].y;
-    // const double dz = particles[1].z - particles[0].z;
-    // const double dr = sqrt(dx*dx + dy*dy + dz*dz); // which is faster?
-    const double dr = po.d;
-
-    const double vx = particles[1].vx;
-    const double vy = particles[1].vy;
-    const double vz = particles[1].vz;
-    // const double vmag = sqrt(vx*vx + vy*vy + vz*vz); // magnitude of velocity (which is faster?)
-    const double vmag = po.v;
-
-    const double M0 = particles[0].m;     // primary mass
-    const double m = particles[1].m;      // particle mass
+    const double dr = po.d;               // particle's radial distance
+    const double vmag = po.v;             // particl's' relative velocity
+    const double M0 = particles[0].m;     // primary's mass
+    const double m = particles[1].m;      // particle's mass
     const double q = m/M0;                // mass ratio
     const double rratio = R0/dr;          // ratio of primary physical radius to orbital radius
-    const double omega = 2.*M_PI/po.P;    // angular velocity of orbiting particle
+    const double omega = po.n;            // angular velocity of orbiting particle
     const double t_f = cbrt(M0*R0*R0/L0); // convective friction time (Zahn 1989, Eq.15)
     
-    // Equation (4) of Schroder & Smith (2008)
-    const double torque = 6.*(lambda2/t_f)*q*q*M0*R0*R0*rratio*rratio*rratio*rratio*rratio*rratio*(Omega - omega); // torque from retarded solar bulges (circular orbit)
+    // Equation (4) of Schroder & Smith (2008):
+    // torque from retarded solar bulges for planar (i.e.
+    // omega and Omega are parallel) and circular orbits
+    const double torque = 6.*(lambda2/t_f)*q*q*M0*R0*R0*rratio*rratio*rratio*rratio*rratio*rratio*(Omega - omega);
+
+    const double prefac = torque/m/dr/vmag; // consolidate prefactor for acceleration components
 
     // Apply torque to acceleration components
-    particles[1].ax += (torque/(m*dr))*(vx/vmag);
-    particles[1].ay += (torque/(m*dr))*(vy/vmag);
-    particles[1].az += (torque/(m*dr))*(vz/vmag);
+    particles[1].ax += prefac*particles[1].vx;
+    particles[1].ay += prefac*particles[1].vy;
+    particles[1].az += prefac*particles[1].vz;
+
+    // Apply proportional (mass ratio, q) reverse torque to primary
+    particles[0].ax -= q*prefac*particles[1].vx;
+    particles[0].ay -= q*prefac*particles[1].vy;
+    particles[0].az -= q*prefac*particles[1].vz;
 }
