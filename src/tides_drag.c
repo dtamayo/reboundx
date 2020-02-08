c0bd209e81708e26d88b64d3de6019107b6ce89b
Stanley A. Baronett <51060809+sabaronett@users.noreply.github.com>
2020-02-07 20:14:12 -0800

    Add primary logic & conv. funcs.

--- src/tides_drag.c
+++ src/tides_drag.c
@@ -36,6 +36,7 @@
  * ======================= ===============================================
  *
  * ADD DESCRIPTION HERE
+ *  ...torque from retarded solar bulges for planar (i.e. omega and Omega are parallel) and circular orbits
  * 
  * **Effect Parameters**
  * 
@@ -46,8 +47,8 @@
  * ============================ =========== ==================================================================
  * Field (C type)               Required    Description
  * ============================ =========== ==================================================================
- * PARAM (float)                Yes         DESCRIPTION
  * primary (int)                No          Set to 1 to specify the primary.  Defaults to treating particles[0] as primary if not set.
+ * PARAM (float)                Yes         DESCRIPTION
  * ============================ =========== ==================================================================
  * 
  */
@@ -58,67 +59,61 @@
 #include <float.h>
 #include "reboundx.h"
 
-void rebx_tides_drag(struct reb_simulation* const sim, struct rebx_force* const force, struct reb_particle* const particles, const int N){
-    //FOR USE LATER W/ REGISTERED PARAMETERS
-    struct rebx_extras* const rebx = sim->extras; // to access add'l params
-    const double* L = rebx_get_param(rebx, particles[0].ap, "luminosity");
-    const double* O = rebx_get_param(rebx, particles[0].ap, "tides_Omega");
-    const double* l2 = rebx_get_param(rebx, particles[0].ap, "tides_lambda2");
-
-    // for (int i=0; i<N; i++){
-    //     const double* migration_tau = rebx_get_param(rebx, particles[i].ap, "migration_tau");
-    //     if (migration_tau != NULL){
-    //         double tau = *migration_tau;
-    //         particles[1].ax -= particles[1].vx/tau;
-    //         particles[1].ay -= particles[1].vy/tau;
-    //         particles[1].az -= particles[1].vz/tau;
-    //     }
-    //     else if (i == 1 && migration_tau == NULL){
-    //         printf("NULL POINTER\n");
-    //     }
-    // }
-
-    if (L == NULL){
-        rebx_error(rebx, "Primary luminosity required. Ignoring effect.");
-    }   
-    else if (O == NULL){
-        rebx_error(rebx, "Primary rotational velocity (Omega) required. Ignoring effect.");
-    }
-    else if (l2 == NULL){
-        rebx_error(rebx, "Primary lambda2 required. Ignoring effect.");
-    }
+static void rebx_calculate_tides_drag(struct rebx_extras* const rebx, struct reb_simulation* const sim, struct reb_particle* const particles, const int N, const int source_index){
+    struct reb_particle* const source = &particles[source_index];
+    double* R  = rebx_get_param(rebx, source->ap, "R_tides");
+    double* L  = rebx_get_param(rebx, source->ap, "luminosity");
+    double* l2 = rebx_get_param(rebx, source->ap, "tides_lambda2");
+    double* O  = rebx_get_param(rebx, source->ap, "tides_Omega");
+    
+    if      (R == NULL)  rebx_error(rebx, "Primary \"R_tides\" (physical radius) required. Ignoring effect.");
+    else if (L == NULL)  rebx_error(rebx, "Primary \"luminosity\" required. Ignoring effect.");   
+    else if (l2 == NULL) rebx_error(rebx, "Primary \"lambda2\" required. Ignoring effect.");
     else{
-        // dereference add'l particle params
-        const double L0 = *L;                 // solar luminosity
-        const double Omega = *O;              // angular velocity of solar rotation
-        const double lambda2 = *l2;           // depends on properties of convective envelope
-        const double R0 = particles[0].r;     // primary physical radius
-        const double M0 = particles[0].m;     // primary mass
-
-        struct reb_orbit po = reb_tools_particle_to_orbit(sim->G, particles[1], particles[0]);
-        const double dr = po.d;               // orbital radius
-        const double vmag = po.v;             // relative velocity
-        const double m = particles[1].m;      // particle mass
-        const double q = m/M0;                // mass ratio
-        const double rratio = R0/dr;          // ratio of primary physical radius to orbital radius
-        const double omega = po.n;            // angular velocity of orbiting particle
-        const double t_f = cbrt(M0*R0*R0/L0); // convective friction time (Zahn 1989, Eq.15)
+        struct reb_orbit po;
+        double m, dr, vmag, omega, q, rratio;
+        double R0 = *R;                 // primary physical radius
+        double L0 = *L;                 // solar luminosity
+        double M0 = source->m;          // primary mass
+        double t_f = cbrt(M0*R0*R0/L0); // convective friction time (Zahn 1989, Eq.15)
+        double lambda2 = *l2;           // depends on properties of convective envelope
+        double Omega = 0;               // angular velocity of solar rotation
+        if (O != NULL) Omega = *O;
         
-        // Equation (4) of Schroder & Smith (2008):
-        // torque from retarded solar bulges for planar (i.e.
-        // omega and Omega are parallel) and circular orbits
-        const double torque = 6.*(lambda2/t_f)*q*q*M0*R0*R0*rratio*rratio*rratio*rratio*rratio*rratio*(Omega - omega);
-
-        const double prefac = torque/m/dr/vmag; // consolidate prefactor for acceleration components
-
-        // Apply torque to acceleration components
-        particles[1].ax += prefac/(1-q)*(particles[1].vx - particles[0].vx);
-        particles[1].ay += prefac/(1-q)*(particles[1].vy - particles[0].vy);
-        particles[1].az += prefac/(1-q)*(particles[1].vz - particles[0].vz);
+        for (int i=0; i<N; i++){
+            if (i == source_index) continue;
+            po = reb_tools_particle_to_orbit(sim->G, particles[i], particles[0]);
+            dr = po.d;                  // orbital radius
+            vmag = po.v;                // relative velocity
+            m = particles[i].m;         // particle mass
+            q = m/M0;                   // mass ratio
+            rratio = R0/dr;             // ratio of primary physical radius to orbital radius
+            omega = po.n;               // angular velocity of orbiting particle
         
-        // Apply proportional (mass ratio, q) reverse torque to primary
-        particles[0].ax -= prefac*q/(1-q)*(particles[1].vx - particles[0].vx);
-        particles[0].ay -= prefac*q/(1-q)*(particles[1].vy - particles[0].vy);
-        particles[0].az -= prefac*q/(1-q)*(particles[1].vz - particles[0].vz);
+            // Equation (4) of Schroder & Smith (2008):
+            const double torque = 6.*(lambda2/t_f)*q*q*M0*R0*R0*rratio*rratio*rratio*rratio*rratio*rratio*(Omega - omega);
+
+            const double prefac = torque/m/dr/vmag;
+
+            particles[i].ax += prefac/(1-q)*(particles[i].vx - source->vx);
+            particles[i].ay += prefac/(1-q)*(particles[i].vy - source->vy);
+            particles[i].az += prefac/(1-q)*(particles[i].vz - source->vz);
+            source->ax -= prefac*q/(1-q)*(particles[i].vx - source->vx);
+            source->ay -= prefac*q/(1-q)*(particles[i].vy - source->vy);
+            source->az -= prefac*q/(1-q)*(particles[i].vz - source->vz);
+        }
     }
+}
+
+void rebx_tides_drag(struct reb_simulation* const sim, struct rebx_force* const force, struct reb_particle* const particles, const int N){
+    struct rebx_extras* const rebx = sim->extras;
+    _Bool source_found = 0;
+    
+    for (int i=0; i<N; i++){
+        if (rebx_get_param(rebx, particles[i].ap, "tides_primary") != NULL){
+            source_found = 1;
+            rebx_calculate_tides_drag(rebx, sim, particles, N, i);
+        }
+    }
+    if (!source_found) rebx_calculate_tides_drag(rebx, sim, particles, N, 0); // default source to index 0 if "tides_primary" not found on any particle
 }
