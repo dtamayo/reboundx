61f34970531086d40a9027dc006e38add380aaf7
Kaltrina Kajtazi <1kaltrinakajtazi@gmail.com>
2021-10-30 15:06:39 +0200

    Small changes, corrections and cleaning up the example files(C and Python)

--- examples/type_I_migration/problem.c
+++ examples/type_I_migration/problem.c
@@ -11,15 +11,12 @@
 
 int main(int argc, char* argv[]){
     struct reb_simulation* sim = reb_create_simulation(); 
-    /* sim.units = ('yr', 'AU', 'Msun') 
+    /* sim.units = ('yr', 'AU', 'Msun') */
+    sim->G = 4*M_PI*M_PI;
 
     struct reb_particle star = {0};  
     star.m     = 1.;   
-    reb_add(sim, star); */
-
-    struct reb_particle p = {0};
-    p.m     = 1.;  // what is one here? Is this the star included??
-    reb_add(sim, p);
+    reb_add(sim, star);
 
     double m = 0.00001;
     double a1 = 0.5;
@@ -30,8 +27,8 @@ int main(int argc, char* argv[]){
     double omega = 0.;
     double f = 0.;
 
-    struct reb_particle p1 = reb_tools_orbit_to_particle(sim->G, p, m, a1, e, inc, Omega, omega, f);
-    struct reb_particle p2 = reb_tools_orbit_to_particle(sim->G, p, m, a2, e, inc, Omega, omega, f);
+    struct reb_particle p1 = reb_tools_orbit_to_particle(sim->G, star, m, a1, e, inc, Omega, omega, f);
+    struct reb_particle p2 = reb_tools_orbit_to_particle(sim->G, star, m, a2, e, inc, Omega, omega, f);
     reb_add(sim, p1);
     reb_add(sim, p2);
     reb_move_to_com(sim);
@@ -42,12 +39,12 @@ int main(int argc, char* argv[]){
     struct rebx_extras* rebx = rebx_attach(sim);
 
     struct rebx_force* type_I_mig = rebx_load_force(rebx, "type_I_migration");
-    rebx_set_param_double(rebx, &force->ap, "inner_disk_edge_position", 0.1); //DO I need to say that it is a double?
-    rebx_set_param_double(rebx, &force->ap, "disk_edge_width", 0.02); //calculated using the scale height value given below
-    rebx_set_param_double(rebx, &force->ap, "flaring_index", 0.25);
-    rebx_set_param_double(rebx, &force->ap, "surface_density_exponent", 1);
-    rebx_set_param_double(rebx, &force->ap, "initial_surface_density", 0.00011255); //in units of Msun/AU^2 which is roughly 1000g/cm^2
-    rebx_set_param_double(rebx, &force->ap, "scale_height", 0.03);
+    rebx_set_param_double(rebx, &type_I_mig->ap, "inner_disk_edge_position", 0.1); //DO I need to say that it is a double?
+    rebx_set_param_double(rebx, &type_I_mig->ap, "disk_edge_width", 0.02); //calculated using the scale height value given below
+    rebx_set_param_double(rebx, &type_I_mig->ap, "flaring_index", 0.25);
+    rebx_set_param_double(rebx, &type_I_mig->ap, "surface_density_exponent", 1);
+    rebx_set_param_double(rebx, &type_I_mig->ap, "initial_surface_density", 0.00011255); //in units of Msun/AU^2 which is roughly 1000g/cm^2
+    rebx_set_param_double(rebx, &type_I_mig->ap, "scale_height", 0.03);
     rebx_add_force(rebx, type_I_mig);
 
     double tmax = 1.e4;  // note that one can calculate the timescale of semi-major axis of the outer planet then set the integration time to twice this value 
