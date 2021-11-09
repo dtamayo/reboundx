b2c33030fe112d482c300df2e0be3f4fae1b5c33
Kaltrina Kajtazi <1kaltrinakajtazi@gmail.com>
2021-11-09 13:07:03 +0100

    Final check of the code files and the example files

--- examples/inner_disk_edge/problem.c
+++ examples/inner_disk_edge/problem.c
@@ -11,15 +11,12 @@
 
 int main(int argc, char* argv[]){
     struct reb_simulation* sim = reb_create_simulation(); 
-    /*sim.units = ('yr', 'AU', 'Msun')
+    /*sim units are ('yr', 'AU', 'Msun')*/
+    sim->G = 4*M_PI*M_PI;
 
-    struct reb_particle star = {0};  
-    star.m     = 1.;   
-    reb_add(sim, star); */
-
-    struct reb_particle p = {0};
-    p.m     = 1.;  // what is one here? Is this the star included??
-    reb_add(sim, p);
+    struct reb_particle star = {0};
+    star.m     = 1.;  
+    reb_add(sim, star);
 
     double m = 0.00001;
     double a = 1;
@@ -29,7 +26,7 @@ int main(int argc, char* argv[]){
     double omega = 0.;
     double f = 0.;
 
-    struct reb_particle p1 = reb_tools_orbit_to_particle(sim->G, p, m, a, e, inc, Omega, omega, f);
+    struct reb_particle p1 = reb_tools_orbit_to_particle(sim->G, star, m, a, e, inc, Omega, omega, f);
     reb_add(sim, p1);
 
     sim->dt = 0.002;  //The period at inner disk edge divided by 20, for a disk edge location at 0.1 AU
@@ -39,7 +36,7 @@ int main(int argc, char* argv[]){
 
     struct rebx_force* inner_edge = rebx_load_force(rebx, "inner_disk_edge");
     rebx_set_param_double(rebx, &inner_edge->ap, "inner_disk_edge_position", 0.1); 
-    rebx_set_param_double(rebx, &inner_edge->ap, "disk_edge_width", 0.02); //calculated using a scale height value of 0.03. See Pichierri et al. 2018 for the equation
+    rebx_set_param_double(rebx, &inner_edge->ap, "disk_edge_width", 0.02); //Calculated using a scale height value of 0.03. See Pichierri et al. 2018 for the equation
     rebx_add_force(rebx, inner_edge);
 
     double tmax = 1.e4; 
