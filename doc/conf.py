1aa46eb0eda597ec4aeda6a490b037e059c804bc
Daniel Tamayo <tamayo.daniel@gmail.com>
2022-06-06 15:55:29 -0400

    3.7.0
    Former-commit-id: 27ea758686420ffb0193a48b1af3e3afc40bbf5d

--- doc/conf.py
+++ doc/conf.py
@@ -200,9 +200,9 @@ master_doc = 'index'
 # built documents.
 #
 # The short X.Y version.
-version = '3.6'
+version = '3.7'
 # The full version, including alpha/beta/rc tags.
-release = '3.6.0'
+release = '3.7.0'
 
 # General information about the project.
 project = u"REBOUNDx ({0})".format(release)
