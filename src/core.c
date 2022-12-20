6f7b4bceacd8fe3dd4b92247e862590f961764b4
Tiger Lu <tigerlu@Tigers-MBP-2.wireless.yale.internal>
2022-12-20 12:57:57 -0500

    changed helper functions and spin->tides_spin
    Former-commit-id: a04f76cfe0dee808036660fa3ebdc891da9c17c6

--- src/core.c
+++ src/core.c
@@ -314,8 +314,8 @@ struct rebx_force* rebx_load_force(struct rebx_extras* const rebx, const char* n
         force->update_accelerations = rebx_modify_orbits_with_type_I_migration;
         force->force_type = REBX_FORCE_VEL;
     }
-    else if (strcmp(name, "spin") == 0){
-        force->update_accelerations = rebx_spin;
+    else if (strcmp(name, "tides_spin") == 0){
+        force->update_accelerations = rebx_tides_spin;
         force->force_type = REBX_FORCE_VEL;
     }
     else if (strcmp(name, "yarkovsky_effect") == 0){
