4bd84171024b7eeff55ac8f0b6331a0a3be9903f
Arya Akmal <akmala@gmail.com>
2023-04-22 10:43:59 -0400

    L-T problem file and bug fixes.
    Former-commit-id: 1a4b34cf9d45d92fdd50817a7b04a3625d553d7e

--- examples/lense_thirring/problem.c
+++ examples/lense_thirring/problem.c
@@ -28,7 +28,21 @@ int main(int argc, char* argv[]){
     struct rebx_force* lense  = rebx_load_force(rebx, "lense_thirring"); // add our new force
     rebx_add_force(rebx, lense);
 
-    rebx_set_param_double(rebx, &sim->particles[1].ap, "p_hat_x", 0.01);
+    rebx_set_param_double(rebx, &sim->particles[0].ap, "lt_rot_rate", 90.361036076);
+    rebx_set_param_double(rebx, &sim->particles[0].ap, "lt_Mom_I_fac", 0.06884);
+    rebx_set_param_double(rebx, &sim->particles[0].ap, "lt_R_eq", 0.00465247264);
+    rebx_set_param_double(rebx, &sim->particles[0].ap, "lt_p_hatx", 0.0);
+    rebx_set_param_double(rebx, &sim->particles[0].ap, "lt_p_haty", 0.0);
+    rebx_set_param_double(rebx, &sim->particles[0].ap, "lt_p_hatz", 1.0);
+
+    // Have to set speed of light in right units (set by G & initial conditions).  Here we use default units of AU/(yr/2pi)
+    rebx_set_param_double(rebx, &lense->ap, "lt_c", 10065.32);
+
+//  rebx_set_param_double(rebx, &sim->particles[1].ap, "ye_body_density", density);
+//  rebx_set_param_int(rebx, &sim->particles[1].ap, "ye_flag", 0);
+//  rebx_set_param_double(rebx, &yark->ap, "ye_lstar", lstar);
+//  rebx_set_param_double(rebx, &yark->ap, "ye_c", c);
+
 
     double tmax = 100000.;
     reb_integrate(sim, tmax);
