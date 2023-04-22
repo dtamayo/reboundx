cb989363016cc35b253c4e5c25d25d4c0b659517
Arya Akmal <akmala@gmail.com>
2023-04-22 12:47:50 -0400

    Minor cleanup

--- examples/lense_thirring/problem.c
+++ examples/lense_thirring/problem.c
@@ -17,6 +17,12 @@ int main(int argc, char* argv[]){
     struct reb_particle star = {0};
     star.m     = 1.;
     reb_add(sim, star);
+    double omega = 90.361036076; //solar rotation rate in rad/year
+    double C_I = 0.06884; //solar moment of inertia prefactor
+    double R_eq = 0.00465247264; //solar equatorial radius in AU
+    double p_x = 0.0; //x-comp. of unit spin-pole direction
+    double p_y = 0.0; //y-comp. of unit spin-pole direction
+    double p_z = 0.0; //z-comp. of unit spin-pole direction
 
 
     struct reb_particle planet = {0};  // add a planet on a circular orbit (with default units where G=1)
@@ -28,20 +34,16 @@ int main(int argc, char* argv[]){
     struct rebx_force* lense  = rebx_load_force(rebx, "lense_thirring"); // add our new force
     rebx_add_force(rebx, lense);
 
-    rebx_set_param_double(rebx, &sim->particles[0].ap, "lt_rot_rate", 90.361036076);
-    rebx_set_param_double(rebx, &sim->particles[0].ap, "lt_Mom_I_fac", 0.06884);
-    rebx_set_param_double(rebx, &sim->particles[0].ap, "lt_R_eq", 0.00465247264);
-    rebx_set_param_double(rebx, &sim->particles[0].ap, "lt_p_hatx", 0.0);
-    rebx_set_param_double(rebx, &sim->particles[0].ap, "lt_p_haty", 0.0);
-    rebx_set_param_double(rebx, &sim->particles[0].ap, "lt_p_hatz", 1.0);
+    rebx_set_param_double(rebx, &sim->particles[0].ap, "lt_rot_rate", omega);
+    rebx_set_param_double(rebx, &sim->particles[0].ap, "lt_Mom_I_fac", C_I);
+    rebx_set_param_double(rebx, &sim->particles[0].ap, "lt_R_eq", R_eq);
+    rebx_set_param_double(rebx, &sim->particles[0].ap, "lt_p_hatx", p_x);
+    rebx_set_param_double(rebx, &sim->particles[0].ap, "lt_p_haty", p_y);
+    rebx_set_param_double(rebx, &sim->particles[0].ap, "lt_p_hatz", p_z);
 
     // Have to set speed of light in right units (set by G & initial conditions).  Here we use default units of AU/(yr/2pi)
     rebx_set_param_double(rebx, &lense->ap, "lt_c", 10065.32);
 
-//  rebx_set_param_double(rebx, &sim->particles[1].ap, "ye_body_density", density);
-//  rebx_set_param_int(rebx, &sim->particles[1].ap, "ye_flag", 0);
-//  rebx_set_param_double(rebx, &yark->ap, "ye_lstar", lstar);
-//  rebx_set_param_double(rebx, &yark->ap, "ye_c", c);
 
 
     double tmax = 100000.;
