e0c5f5792a1ce188d1ec4da0f7ddf38020f3392c
HaniyehTajer <tajer.1@osu.edu>
2026-01-05 13:27:40 -0500

    python example

--- examples/fragmenting_collisions_python/fc_file_name_test.py
+++ examples/fragmenting_collisions_python/fc_file_name_test.py
@@ -9,8 +9,7 @@ rebx = reboundx.Extras(sim)
 collision_resolve = rebx.load_collision_resolve("fragmenting_collisions")
 rebx.add_collision_resolve(collision_resolve)
 collision_resolve.params["fc_min_frag_mass"] = 1.0
-# keep a ctypes c_char_p instance on the collision_resolve object so the C code receives a ctypes pointer
-collision_resolve._fc_particle_list_file_c = c_char_p(b"family_tree.csv")
+
 collision_resolve.params["fc_particle_list_file"] = c_char_p(b"family_tree.csv")
 
 sim.add(m=1, r=1)
