3c4305986ba51b4f17c0a1177966a4107b57678f
HaniyehTajer <tajer.1@osu.edu>
2026-01-07 16:46:14 -0500

    docs completed

--- src/fragmenting_collisions.c
+++ src/fragmenting_collisions.c
@@ -29,12 +29,23 @@
  * 
  * ======================= ===============================================
  * Authors                 H. Tajer, H. Rein, T. Lu
- * Based on                None
+ * Implementation Paper    `In prep.`_.
+ * Based on                `Leinhardt and Stewart 2012 <https://iopscience.iop.org/article/10.1088/0004-637X/745/1/79>`_, `Chambers 2013 <https://www.sciencedirect.com/science/article/pii/S0019103513000754?via%3Dihub>`_.
  * C Example               :ref:`c_example_fragmenting_collisions_embryo_disk`
+ * Python Example          `FragmentingCollisions.ipynb <https://github.com/dtamayo/reboundx/blob/master/ipython_examples/FragmentingCollisions.ipynb>`_.
  * ======================= ===============================================
  * 
- * This is an example implementation of a REBOUNDx collision module.
- * The outcome is based on the collision prescription of Leinhardt & Stewart (2012), from now on called LS2012
+ * This module is based on the collision prescription of Leinhardt & Stewart (2012), from now on called LS2012. 
+ * After a collision between two bodies is detected, the outcome will depend on the velocity of the collision, mass of the bodies, and the impact angle.
+ * Possible outcomes are:
+ * 
+ * 1. "merging": particles will merge 
+ * 
+ * 2. "fragmentation": erosion of the target or accretion of projectile material into target (fragmentation)
+ * 
+ * 3. "elastic bounce": particles will leave each other intact
+ * 
+ * For more details, refer to the implementation paper (in prep).
  * 
  * **Effect Parameters**
  * 
