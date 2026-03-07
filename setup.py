d3433130df5a0939c83043df38cb9cd117487189
Hanno Rein <hanno@hanno-rein.de>
2026-03-07 12:43:19 -0500

    more setup cleanup

--- setup.py
+++ setup.py
@@ -103,33 +103,9 @@ libreboundxmodule = Extension('libreboundx',
                     )
 
 setup(name='reboundx',
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 5 - Production/Stable',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'Topic :: Scientific/Engineering :: Astronomy',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-    ],
-    keywords='astronomy astrophysics nbody integrator',
     packages=['reboundx'],
     cmdclass={'build_ext':build_ext},
     setup_requires=['rebound>=5.0.0'],
     install_requires=['rebound>=5.0.0'],
-    tests_require=['rebound>=5.0.0','numpy'],
-    test_suite="reboundx.test",
     ext_modules = [libreboundxmodule],
     zip_safe=False)
